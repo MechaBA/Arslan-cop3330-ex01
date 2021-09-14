672f6262b1d5299fe70fe340c7dcf2766c7edd5a
MechaBA <berkay_arslan@live.com>
2021-09-13 22:09:32 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Berkay Arslan
+ */
+
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.print("What is your name? Berkay \n");
+        System.out.println("Hello, Berkay, nice to meet you!");
+    }
+}
